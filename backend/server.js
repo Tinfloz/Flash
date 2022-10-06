7bf8d8c854c9140d28778f0d15c1ddd2ab7d2a46
Tinfloz <swapnonil0007@gmail.com>
2022-10-06 18:49:00 -0400

    connected to mongo db

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser'
+import mongoose from 'mongoose';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 9001;
+
+//connect to mongo db
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(res => {
+    console.log("connect to the database");
+}).catch(err => {
+    console.error(err);
+});
 
 //middlewares
 app.use(express.json());
