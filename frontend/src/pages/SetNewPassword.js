8f37e181d9f8ba308e6d2ac82a6fd6a21feca06f
Tinfloz <swapnonil0007@gmail.com>
2022-11-24 12:36:27 -0500

    add reset password ui

---
+++ frontend/src/pages/SetNewPassword.js
@@ -0,0 +1,108 @@
+import { Box, Button, Flex, Text, VStack, Input, useToast } from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { resetHelpers, resetUserPassword } from '../reducers/user/userSlice';
+
+const SetNewPassword = () => {
+
+    const { resetToken } = useParams();
+    console.log(resetToken)
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const toast = useToast();
+    const { message, isSuccess, isError } = useSelector(state => state.user);
+    const [resetPassword, setResetPassword] = useState({
+        newPassword: "",
+        confirmNewPassword: ""
+    });
+
+    const handleChange = (e) => {
+        setResetPassword(prevState => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+    };
+
+    useEffect(() => {
+        if (!isSuccess && !isError) {
+            return
+        };
+        if (isSuccess) {
+            toast({
+                position: "bottom-left",
+                title: "Success",
+                description: message,
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+            });
+            dispatch(resetHelpers());
+            setTimeout(() => navigate("/login"), 3000)
+        };
+        if (isError) {
+            toast({
+                position: "bottom-left",
+                title: "Error",
+                description: message,
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+            });
+            dispatch(resetHelpers());
+        }
+    }, [isSuccess, isError, message, toast, dispatch])
+
+    return (
+        <>
+            <Flex
+                justify="center"
+                alignItems="center"
+                h="80vh"
+            >
+                <Box
+                    w="50vh"
+                    h="50vh"
+                    borderWidth="1px"
+                    borderColor="gray.300"
+                >
+                    <Flex
+                        justify="center"
+                        alignItems="center"
+                        p="8vh"
+                    >
+                        <VStack spacing="2vh">
+                            <Text as="b" fontSize="3vh"> Enter new password</Text>
+                            <Input placeholder="Enter new password"
+                                type="password"
+                                value={resetPassword.newPassword} name="newPassword"
+                                onChange={handleChange}
+                            />
+                            <Input placeholder="Confirm new password"
+                                type="password"
+                                value={resetPassword.confirmNewPassword} name="confirmNewPassword"
+                                onChange={handleChange}
+                            />
+                            <Button
+                                onClick={async () => {
+                                    let resetDetails = {
+                                        resetToken,
+                                        resetPassword
+                                    };
+                                    await dispatch(resetUserPassword(resetDetails));
+                                    setResetPassword(prevState => ({
+                                        ...prevState,
+                                        newPassword: "",
+                                        confirmNewPassword: ""
+                                    }));
+                                }}
+                            >Reset</Button>
+                        </VStack>
+                    </Flex>
+                </Box>
+            </Flex >
+        </>
+    )
+}
+
+export default SetNewPassword
